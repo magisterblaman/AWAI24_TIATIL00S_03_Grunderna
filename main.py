5bee66abe659439ff0e462c14444803f7334003f
magisterblaman <hannes.blaman@lbs.se>
2024-09-17 09:28:27 +0200

    Övningar 1 - Checkout lösning

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-school = input("Vilken skola går du på? ")
+player1 = input("Spelare 1: Sten, sax, eller påse?").lower().strip()
+print("\n" * 100)
+player2 = input("Spelare 2: Sten, sax, eller påse?").lower().strip()
 
-if school == "lbs":
-    print("Har du en stabil mental hälsa? ")
-    mental_health = input().strip()
-    if mental_health == "ja":
-        print("Byt skola")
-    else:
-        print("Alexandra sitter på övervåningen")
-elif school == "pb":
-    pass
-elif school == "ed":
-    pass
+if ((player1 == "sax" and player2 == "påse")
+        or (player1 == "sten" and player2 == "sax")
+        or (player1 == "påse" and player2 == "sten")):
+    print("Spelare 1 vinner")
+elif ((player2 == "sax" and player1 == "påse")
+        or (player2 == "sten" and player1 == "sax")
+        or (player2 == "påse" and player1 == "sten")):
+    print("Spelare 2 vinner")
 else:
-    print("Du har en konstig skola")
+    print("Oavgjort")
